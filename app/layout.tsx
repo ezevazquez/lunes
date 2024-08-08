1444c53a27000dd0da095741742aa2f8a4843f2d
Eze Vazquez <eze@Ezes-Air.fibertel.com.ar>
2024-08-08 11:49:24 -0300

    agrego metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,8 +3,8 @@ import './globals.css';
 import Head from 'next/head';
 
 export const metadata = {
-  title: 'Renuncio hoy',
-  description: 'Los lunes son de renuncia',
+  title: 'Canción de los Lunes',
+  description: 'Los lunes se renuncia :telegrama:',
 };
 
 export default function RootLayout({
@@ -16,6 +16,14 @@ export default function RootLayout({
     <html lang="es">
       <Head>
         <link rel="icon" href="/favicon.ico" />
+        {/* Meta Tags de Open Graph para Slack */}
+        <meta property="og:type" content="music.song" />
+        <meta property="og:title" content="Canción de los Lunes 🎵" />
+        <meta property="og:description" content="Los lunes se renuncia :telegrama:" />
+        <meta property="og:url" content="https://lunes.probandocosas.com" />
+        <meta property="og:image" content="https://lunes.probandocosas.com/thumbnail.jpg" />
+        <meta property="og:audio" content="https://lunes.probandocosas.com/audio/Renuncio.mp3" />
+        <meta property="og:audio:type" content="audio/mp3" />
       </Head>
       <body>{children}</body>
     </html>
